fb1e3acd4e9316be2588588f913f65d78a038660
blake <blakestrebel2018@u.northwestern.edu>
2016-12-03 16:50:15 -0600

    updated camera position

--- src/leftarm_setup.py
+++ src/leftarm_setup.py
@@ -4,7 +4,7 @@ import baxter_interface
 
 def camera_setup():
     limb = baxter_interface.Limb('left')
-    home = {'left_w0': 1.3081021168692866, 'left_w1': 2.091966299478733, 'left_w2': 1.1140535472017816, 'left_e0': -1.3410827038088229, 'left_e1': 0.09740778003072377, 'left_s0': -1.1136700520048104, 'left_s1': -0.5184855063052698}
+    home = {'left_w0': 1.3679273675968178, 'left_w1': 1.8338740319170121, 'left_w2': -0.1234854534247758, 'left_e0': -1.3591069780664766, 'left_e1': -0.04410194765170564, 'left_s0': -0.8881748761856546, 'left_s1': -0.27074760906177553}
     limb.move_to_joint_positions(home)
 
 if __name__ == '__main__':
