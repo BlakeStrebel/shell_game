ff8c1efe68453500d427f67cb2203b8a77c29cc9
Adam Pollack <apollack11@gmail.com>
2016-12-03 19:20:31 -0600

    Cup tracking node tracks the location and identity of 3 cups

--- src/cup_tracking.py
+++ src/cup_tracking.py
@@ -6,22 +6,31 @@ import numpy as np
 from geometry_msgs.msg import Point
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from collections import deque
+from scipy.spatial import distance
+
+class cup:
+    def __init__(self):
+        self.containsTreasure = False
+        self.originalPoint = 0
+        self.currentPoint = 0
 
 class image_converter:
     def __init__(self):
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
-        self.pts1 = deque(maxlen=32)
-        self.pts2 = deque(maxlen=32)
-        self.pts3 = deque(maxlen=32)
-        self.deltaValues = Point()
+        self.cups = []
+        self.cupCenters = [[0,0],[0,0],[0,0]]
+        self.flag = False
+        self.minRadius = 30
+        for i in range(0,3):
+            self.cups.append(cup())
+            print self.cups[i].containsTreasure
 
     def callback(self,data):
         try:
-            imgOriginal = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            imgOriginal = self.bridge.imgmsg_to_cv2(data,"bgr8")
         except CvBridgeError as e:
-            print("==[CAMERA MANAGER]==", e)
+            print("==[CAMERA MANAGER]==",e)
 
         blurred = cv2.GaussianBlur(imgOriginal,(11,11),0)
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
@@ -35,50 +44,59 @@ class image_converter:
         outputGrayscale = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
         contours = cv2.findContours(outputGrayscale,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
 
+        radius = [0,0,0]
+        x = [[0,0],[0,0],[0,0]]
+        y = [[0,0],[0,0],[0,0]]
         if len(contours) > 2:
             contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
-            c1 = contours[0]
-            c2 = contours[1]
-            c3 = contours[2]
-            ((x1,y1),radius1) = cv2.minEnclosingCircle(c1)
-            ((x2,y2),radius2) = cv2.minEnclosingCircle(c2)
-            ((x3,y3),radius3) = cv2.minEnclosingCircle(c3)
-            M1 = cv2.moments(c1)
-            M2 = cv2.moments(c2)
-            M3 = cv2.moments(c3)
-            ballCenter1 = (int(M1["m10"] / M1["m00"]), int(M1["m01"] / M1["m00"]))
-            ballCenter2 = (int(M2["m10"] / M2["m00"]), int(M2["m01"] / M2["m00"]))
-            ballCenter3 = (int(M3["m10"] / M3["m00"]), int(M3["m01"] / M3["m00"]))
-
-            if radius1 > 10:
-                cv2.circle(imgOriginal,(int(x1),int(y1)),int(radius1),(0,255,0),2)
-                cv2.circle(imgOriginal,ballCenter1,5,(0,0,255),-1)
-                self.pts1.appendleft(ballCenter1)
-                for i in np.arange(1,len(self.pts1)):
-                    thickness1 = int(np.sqrt(32/float(i+1))*2.5)
-                    cv2.line(imgOriginal,self.pts1[i-1],self.pts1[i],(0,0,255),thickness1)
-
-            if radius2 > 10:
-                cv2.circle(imgOriginal,(int(x2),int(y2)),int(radius2),(0,255,0),2)
-                cv2.circle(imgOriginal,ballCenter2,5,(0,0,255),-1)
-                self.pts2.appendleft(ballCenter2)
-                for i in np.arange(1,len(self.pts2)):
-                    thickness2 = int(np.sqrt(32/float(i+1))*2.5)
-                    cv2.line(imgOriginal,self.pts2[i-1],self.pts2[i],(0,0,255),thickness2)
-
-            if radius3 > 10:
-                cv2.circle(imgOriginal,(int(x3),int(y3)),int(radius3),(0,255,0),2)
-                cv2.circle(imgOriginal,ballCenter3,5,(0,0,255),-1)
-                self.pts3.appendleft(ballCenter3)
-                for i in np.arange(1,len(self.pts3)):
-                    thickness3 = int(np.sqrt(32/float(i+1))*2.5)
-                    cv2.line(imgOriginal,self.pts3[i-1],self.pts3[i],(0,0,255),thickness3)
+            for i in range(0,3):
+                ((x[i],y[i]),radius[i]) = cv2.minEnclosingCircle(contours[i])
+            # this will ensure that it is the 3 separate cups that are being identified
+            # may have to change the radius threshold when testing on baxter
+            if radius[0] > self.minRadius and radius[1] > self.minRadius and radius[2] > self.minRadius:
+                for i in range(0,3):
+                    M = cv2.moments(contours[i])
+                    self.cupCenters[i] = (int(M["m10"]/M["m00"]),int(M["m01"]/M["m00"]))
+                    cv2.circle(imgOriginal,(int(x[i]),int(y[i])),int(radius[i]),(0,255,0),2)
+                    cv2.circle(imgOriginal,self.cupCenters[i],5,(0,0,255),-1)
+                if self.flag is False:
+                    for i in range(0,3):
+                        self.cups[i].originalPoint = self.cupCenters[i]
+                        self.cups[i].currentPoint = self.cupCenters[i]
+                    self.flag = True
+                else:
+                    for i in range(0,3):
+                        print "BEFORE"
+                        print "Cup 1 Current Location:",self.cups[0].currentPoint
+                        print "Cup 2 Current Location:",self.cups[1].currentPoint
+                        print "Cup 3 Current Location:",self.cups[2].currentPoint
+                        print "cupCenter1:",self.cupCenters[0]
+                        print "cupCenter2:",self.cupCenters[1]
+                        print "cupCenter3:",self.cupCenters[2]
+                        self.cups[i].currentPoint = self.cupCenters[closestPoint(self.cups[i].currentPoint,self.cupCenters)]
+                        print "AFTER"
+                        print "Cup 1 Current Location:",self.cups[0].currentPoint
+                        print "Cup 2 Current Location:",self.cups[1].currentPoint
+                        print "Cup 3 Current Location:",self.cups[2].currentPoint
+        else:
+            self.flag = False
 
         flipped = cv2.flip(imgOriginal, 1)
         output = cv2.flip(output, 1)
-        cv2.imshow("Image", flipped)
-        cv2.imshow("MyImage", output)
+        cv2.imshow("Image",flipped)
+        cv2.imshow("MyImage",output)
         cv2.waitKey(3)
+
+def closestPoint(currentPoint,cupCenters):
+    minimumDist = distance.euclidean(currentPoint,cupCenters[0])
+    mindex = 0
+    for i in range(1,3):
+        dist = distance.euclidean(currentPoint,cupCenters[i])
+        if dist < minimumDist:
+            minimumDist = dist
+            mindex = i
+    print "MINDEX:",mindex
+    return mindex
 
 def main(args):
     rospy.init_node('image_converter', anonymous=True)
@@ -92,14 +110,3 @@ def main(args):
 
 if __name__ == '__main__':
     main(sys.argv)
-
-#
-# import numpy as np
-# import cv2
-#
-# # Load an color image in grayscale
-# img = cv2.imread('feature_screen.png',0)
-#
-# cv2.imshow('image',img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
