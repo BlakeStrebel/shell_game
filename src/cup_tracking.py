1a218d63425bfda63a37bd9c4c49ca5a21da271f
Adam Pollack <apollack11@gmail.com>
2016-12-04 13:00:59 -0600

    Cup tracking publishes to treasure_point

--- src/cup_tracking.py
+++ src/cup_tracking.py
@@ -17,14 +17,18 @@ class cup:
 class image_converter:
     def __init__(self):
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
+        self.image_sub = rospy.Subscriber("/cameras/left_hand_camera/image",Image,self.callback)
+        self.treasure_pub = rospy.Publisher("treasure_point",Point,queue_size=10)
+        self.treasure_point = Point()
         self.cups = []
         self.cupCenters = [[0,0],[0,0],[0,0]]
         self.flag = False
-        self.minRadius = 30
+        self.minRadius = 10
         for i in range(0,3):
             self.cups.append(cup())
             print self.cups[i].containsTreasure
+
+        self.cups[0].containsTreasure = True
 
     def callback(self,data):
         try:
@@ -42,7 +46,11 @@ class image_converter:
         mask = cv2.dilate(mask, None, iterations=7)
         output = cv2.bitwise_and(imgOriginal, imgOriginal, mask = mask)
         outputGrayscale = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-        contours = cv2.findContours(outputGrayscale,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
+
+        if major_ver == '3':
+            contours = cv2.findContours(outputGrayscale,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
+        elif major_ver == '2':
+            contours = cv2.findContours(outputGrayscale,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[0]
 
         radius = [0,0,0]
         x = [[0,0],[0,0],[0,0]]
@@ -57,9 +65,8 @@ class image_converter:
                 for i in range(0,3):
                     M = cv2.moments(contours[i])
                     self.cupCenters[i] = (int(M["m10"]/M["m00"]),int(M["m01"]/M["m00"]))
-                    cv2.circle(imgOriginal,(int(x[i]),int(y[i])),int(radius[i]),(0,255,0),2)
-                    cv2.circle(imgOriginal,self.cupCenters[i],5,(0,0,255),-1)
                 if self.flag is False:
+                    print "RESET"
                     for i in range(0,3):
                         self.cups[i].originalPoint = self.cupCenters[i]
                         self.cups[i].currentPoint = self.cupCenters[i]
@@ -74,6 +81,13 @@ class image_converter:
                         print "cupCenter2:",self.cupCenters[1]
                         print "cupCenter3:",self.cupCenters[2]
                         self.cups[i].currentPoint = self.cupCenters[closestPoint(self.cups[i].currentPoint,self.cupCenters)]
+                        if i==0:
+                            cv2.circle(imgOriginal,(int(self.cups[i].currentPoint[0]),int(self.cups[i].currentPoint[1])),int(radius[i]),(255,0,0),2)
+                        if i==1:
+                            cv2.circle(imgOriginal,(int(self.cups[i].currentPoint[0]),int(self.cups[i].currentPoint[1])),int(radius[i]),(0,255,0),2)
+                        if i==2:
+                            cv2.circle(imgOriginal,(int(self.cups[i].currentPoint[0]),int(self.cups[i].currentPoint[1])),int(radius[i]),(0,0,255),2)
+                        cv2.circle(imgOriginal,self.cups[i].currentPoint,5,(0,0,255),-1)
                         print "AFTER"
                         print "Cup 1 Current Location:",self.cups[0].currentPoint
                         print "Cup 2 Current Location:",self.cups[1].currentPoint
@@ -81,6 +95,16 @@ class image_converter:
         else:
             self.flag = False
 
+        print "Cup 1 Original Location:",self.cups[0].originalPoint
+        print "Cup 2 Original Location:",self.cups[1].originalPoint
+        print "Cup 3 Original Location:",self.cups[2].originalPoint
+
+        for i in range(0,3):
+            if self.cups[i].containsTreasure:
+                print "Cup #",i,"CONTAINS TREASURE"
+                self.treasure_point.x = self.cups[i].currentPoint[0]
+                self.treasure_point.y = self.cups[i].currentPoint[1]
+                self.treasure_pub.publish(self.treasure_point)
         flipped = cv2.flip(imgOriginal, 1)
         output = cv2.flip(output, 1)
         cv2.imshow("Image",flipped)
@@ -109,4 +133,6 @@ def main(args):
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+    print "OpenCV Major Version:",major_ver
     main(sys.argv)
