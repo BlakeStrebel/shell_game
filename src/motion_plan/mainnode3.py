5acc58e6332da45031df91120cc66286e970e9e1
wvismaya <wvismaya@users.noreply.github.com>
2016-12-04 18:16:22 -0600

    Change in pixel to physical

--- src/motion_plan/mainnode3.py
+++ src/motion_plan/mainnode3.py
@@ -4,8 +4,6 @@ import rospy
 import planningnode as pnode
 from gripnode import GripperClient
 
-from shell_game import MsgType
-
 import rospy
 import image_geometry
 from sensor_msgs.msg import CameraInfo
@@ -45,26 +43,10 @@ def testnode(data):
 
     gc = GripperClient()
 
-    camera_model = image_geometry.PinholeCameraModel()
-
-    cameraTopic = "/cameras/left_hand_camera/camera_info"
-    pixelTopic = "/shell_game/pixel_point"
-    cameraStateTopic = "/robot/limb/left/endpoint_state"
-
-    rospy.Subscriber(cameraTopic, CameraInfo, initCamera)
-    #rospy.Subscriber(pixelTopic, Point, getPixel)
-    rospy.Subscriber(cameraStateTopic, EndpointState, getCameraState)
-
-    rate = rospy.Rate(50)
-    while(cameraInfo is None) or (cameraStateInfo is None):
-        rate.sleep()
-
-    camera_x = cameraStateInfo.pose.position.x
-    camera_y = cameraStateInfo.pose.position.y
-
-    camera_model.fromCameraInfo(cameraInfo)
     print data
-    coords = convertTo3D(data[0],data[1], camera_model, camera_x, camera_y) #[0.48, -0.3]#
+    print ((data.x-640)*0.0023*0.24 + 0.72 + 0.2)
+    print ((data.y-400)*0.0023*0.24 + 0.17 + 0.2)
+    coords = [ ((data.x-640)*0.0015*0.24 + 0.769 + 0.02), ((data.y-400)*0.0015*0.24 + 0.03 + 0.02)]
     print coords
     rospy.sleep(2)
 
@@ -77,6 +59,8 @@ def testnode(data):
 
     print "Lets pick up the cup"
 
+    
+
     gc.command(position=100.0, effort=50.0)
     gc.wait()
     rospy.sleep(2)
@@ -86,7 +70,7 @@ def testnode(data):
     pnode.initplannode(dpick)
     rospy.sleep(2)
 
-    gc.command(position=4.0, effort=50.0)
+    gc.command(position=70.0, effort=50.0)
     gc.wait()
 
     pnode.initplannode(dsafe)
@@ -99,12 +83,11 @@ def testnode(data):
     gc.command(position=100.0, effort=50.0)
     gc.wait()
 
+    
+
     return
 
 if __name__ == '__main__':
     rospy.init_node('plannode', log_level=rospy.INFO)
-    while():
-        raw_input("Press ENTER to continue.")
-        print "Gotcha!"
-        rospy.Subscriber("nameMessage", MsgType, testnode)
+    rospy.Subscriber("/treasure_point", Point, testnode)
     rospy.spin()
