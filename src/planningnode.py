42e34cacd435d73d17ce936b0011b2d94cea9dbf
wvismaya <wvismaya@users.noreply.github.com>
2016-12-09 04:45:45 -0600

    FINAL: Update planningnode.py for submission

--- src/planningnode.py
+++ src/planningnode.py
@@ -3,16 +3,24 @@
 import struct
 import numpy as np
 
+# REQUIRED TO CALL ROS PACKAGES
 import rospy
+
+# REQUIRED FOR PLANNING
 import moveit_commander
 
+# MESSAGE TYPES
 from std_msgs.msg import Header
 
+# TO CALL IK_SOLVER SERVICE
 from baxter_core_msgs.srv import (
     SolvePositionIK,
     SolvePositionIKRequest)
+
+# TO WRITE TO BAXTER
 import baxter_interface
 
+# MESSAGE TYPES
 from sensor_msgs.msg import JointState
 
 from geometry_msgs.msg import (
@@ -21,58 +29,55 @@ from geometry_msgs.msg import (
     Point,
     Quaternion)
 
-def initplannode(des_pose, limb):
-    print "\r\nTesting position 1"
-    print des_pose
-    move_pos(des_pose, limb)
+# TO USE AS A CALLBACK NODE IN ANOTHER PYTHON FILE
+def initplannode(goal, limb):
+    print "Inside Planning Node"
+    print goal
+    
+    # Actual node that solves IK and calls the MoveIt! planner
+    # in joint-space
+    move_pos(goal, limb)
     return
 
-def ik_timeout(req, limb, timeout=5.0):
+# Uses Baxter's IK service to find the required joint angles,
+# It then calls MoveIT! planners to plan to these joint angles
+def move_pos(goal, limb, timeout=3):
+    # Call Baxter's inbuilt service
     ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
     iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
-    base = rospy.Time.now()
-    # set seed:
-    req.seed_mode = req.SEED_USER
-    right = baxter_interface.Limb('right')
-    while (rospy.Time.now()-base).to_sec() <= timeout:
-        # generate a random q:
-        q = right.joint_angles()
-        js = JointState(name=q.keys(), position=q.values() + np.pi/2*np.random.randn(7))
-        req.seed_angles = np.array([js])
-        try:
-            rospy.wait_for_service(ns, 0.5)
-            resp = iksvc(req)
-        except (rospy.ServiceException, rospy.ROSException), e:
-            rospy.loginfo("Service exception")
-            return None
-        resp_seeds = struct.unpack('<%dB' % len(resp.result_type),
-                               resp.result_type)
-        if (resp_seeds[0] != resp.RESULT_INVALID):
-            break
-    return resp
-
-def move_pos(des_pose, limb, timeout=3):
-    #right_arm_group = moveit_commander.MoveGroupCommander("right_arm")
-    ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
-    iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
+    
+    # Request for same service
     ikreq = SolvePositionIKRequest()
+    
+    # Fill in all message parts
     hdr = Header(stamp=rospy.Time.now(), frame_id='base')
-    #quat1 = quaternion_from_euler(des_pose[3],des_pose[4],des_pose[5])
+    
+    # Define  required x, y, z parts
     pose = Pose()
+    
+    # Define orientation of gripper.
     quat = Quaternion()
-    quat.x = des_pose[3]
-    quat.y = des_pose[4]
-    quat.z = des_pose[5]
-    quat.w = des_pose[6]
+    quat.x = goal[3]
+    quat.y = goal[4]
+    quat.z = goal[5]
+    quat.w = goal[6]
+    
+    # Fill the position vectors from goal configuration
     pose.orientation = quat
-    pose.position.x = des_pose[0]
-    pose.position.y = des_pose[1]
-    pose.position.z = des_pose[2]
+    pose.position.x = goal[0]
+    pose.position.y = goal[1]
+    pose.position.z = goal[2]
+    
     ikreq.pose_stamp = [PoseStamped(header=hdr, pose=pose)]
-    # print ikreq
+    
+    # Using Random seeds till a "VALID" IK solution is found
+    # FOR SEED METHOD:
+    # cite1: IK_Service_Client in package: Baxter_Examples
+    # cite2: test_moveit.py from NXR repo linked in Course notes
     try:
         rospy.wait_for_service(ns, 5.0)
-        resp = iksvc(ikreq) #Response that returns possible joint state values. Throws error on timeout.
+        resp = iksvc(ikreq)        
+        .
     except (rospy.ServiceException, rospy.ROSException), e:
         rospy.loginfo("Service exception")
 
@@ -88,33 +93,52 @@ def move_pos(des_pose, limb, timeout=3):
               (seed_str))
         # Format solution into Limb API-compatible dictionary
         limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
-        print "\nIK Joint Solution:\n", limb_joints
-        print "------------------"
-        print "Response Message:\n", resp
-
+        
+        # Initialize joint angles
         des_joints = [0]*7
+        
+        # Update all joint angles
         for i in range(7):
             des_joints[i] = resp.joints[0].position[i]
+        
+        # Begin interaction with move_it joint_action_server.
+        # Object of type move_group_commander is used to
+        # interact with Baxter using joint_action_server
         ra = moveit_commander.MoveGroupCommander("right_arm")
-        ra.set_joint_value_target(des_joints)
+        
+        # Set desired joint angles to plan in joint space
+        ra.set_joint_value_target(goal)
+        
+        # FOR JOINT SPACE PLANNING:
+        # cite1: test_moveit.py from NXR repo linked in Course notes
         ra.plan()
+        
+        # Sets Baxter arm in Motion
         ra.go()
+        
     else:
-        print("INVALID POSE - No Valid Joint Solution Found.")
+        print("INVALID POSE")
         print("Trying random seeds until timeout is reached")
         ikt = ik_timeout(ikreq, limb, 5)
-        print "\r\n","ikt = "
-        print ikt
-        print ""
         if ikt is not None and len(ikt.joints[0].position) != 0:
             des_joints = [0]*7
             for i in range(7):
                 print i
                 des_joints[i] = ikt.joints[0].position[i]
+            
+            # Same moveit! calling strategy as is written in if code block
             ra = moveit_commander.MoveGroupCommander("right_arm")
             ra.set_joint_value_target(des_joints)
             ra.plan()
             ra.go()
+            
         else:
-            print "-------------EMPTY LIST or NOT A VALID POSITION------------"
+            print "-------------INVALID POSITION------------"
     return
+
+# REFERENCES:
+# test_moveit.py from NXR LAB BAXTER REPO (Linked in course notes and README file of this repo)
+# baxter_examples package --> ik_service_client.py example which 
+# ... tries to find ik solution using Baxter's inbult ik service and 
+# ... trying to find valid solution by using random trials of intial conditions
+# ... called "RANDOM SEED" in this particular reference
